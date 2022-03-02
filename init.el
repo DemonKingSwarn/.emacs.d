a076dde2dee222601f45514054a7847376284007
DemonKingSwarn <rockingswarn@gmail.com>
2022-03-02 19:14:29 +0530

    new config

--- init.el
+++ init.el
@@ -11,6 +11,15 @@
 
 (set-face-attribute 'default nil :font "JetBrainsMono" :height 130)
 
+(setq make-backup-files nil)
+(setq auto-save-default nil)
+
+(defalias 'yes-or-no-p 'y-or-n-p)
+
+(use-package async
+  :ensure t
+  :init (dired-async-mode 1))
+
 ;; make ESC quit prompts
 (global-set-key (kbd "<escape>") 'keyboard-escape-quit)
 
@@ -121,7 +130,17 @@
   (visual-fill-column-mode 1))
 
   (use-package visual-fill-column
-  :hook (org-mode . efs/org-mode-visual-fill))
+    :hook (org-mode . efs/org-mode-visual-fill))
+
+  (push '("conf-unix" . conf-unix) org-src-lang-modes))
+
+(with-eval-after-load 'org
+  ;; This is needed as of Org 9.2
+  (require 'org-tempo)
+  
+  (add-to-list 'org-structure-template-alist '("sh" . "src shell"))
+  (add-to-list 'org-structure-template-alist '("el" . "src emacs-lisp"))
+  (add-to-list 'org-structure-template-alist '("py" . "src python")))
   
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
