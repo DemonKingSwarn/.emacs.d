47d5e291a4b39eb58dde3d74eebf3bfa7fd9dadc
DemonKingSwarn <swarnaditya.7@proton.me>
2026-05-13 12:10:12 +0530

    LOL!
     an automated commit

--- init.el
+++ init.el
@@ -1,7 +1,27 @@
 ;;; GC tuning
 (setq gc-cons-threshold (* 50 1000 1000))
 
+(require 'package)
+(add-to-list 'package-archives '("gnu" . "https://elpa.gnu.org/packages/") t)
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
+(package-initialize)
 
-(require 'use-package)
+(defun package--save-selected-packages (&rest opt) nil)
+
+(defvar bootstrap-version)
+(let ((bootstrap-file
+       (expand-file-name "straight/repos/straight.el/bootstrap.el" user-emacs-directory))
+      (bootstrap-version 6))
+  (unless (file-exists-p bootstrap-file)
+    (with-current-buffer
+        (url-retrieve-synchronously
+         "https://raw.githubusercontent.com/radian-software/straight.el/develop/install.el"
+         'silent 'inhibit-cookies)
+      (goto-char (point-max))
+      (eval-print-last-sexp)))
+  (load bootstrap-file nil 'nomessage))
+
+(straight-use-package 'use-package)
+(setq straight-use-package-by-default t)(require 'use-package)
 
 (org-babel-load-file (concat user-emacs-directory "configuration.org"))
